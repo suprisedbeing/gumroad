4e14caf10b134115c218b4a88e6ed0174390df5e
Amar <rn.drive.2024.1@gmail.com>
2025-12-16 12:53:44 +0530

    requested changes done

--- app/controllers/help_center/articles_controller.rb
+++ app/controllers/help_center/articles_controller.rb
@@ -6,29 +6,29 @@ class HelpCenter::ArticlesController < HelpCenter::BaseController
   layout "inertia"
 
   def index
+    @title = "Gumroad Help Center"
+    @canonical_url = help_center_root_url
+    @description = "Common questions and support documentation"
+
     render inertia: "HelpCenter/Articles/Index",
            props: {
              categories: HelpCenter::Category.all.map { |category| category_props(category) },
              recaptcha_site_key: user_signed_in? ? nil : GlobalConfig.get("RECAPTCHA_LOGIN_SITE_KEY")
            }
-
-    @title = "Gumroad Help Center"
-    @canonical_url = help_center_root_url
-    @description = "Common questions and support documentation"
   end
 
   def show
     @article = HelpCenter::Article.find_by!(slug: params[:slug])
 
+    @title = "#{@article.title} - Gumroad Help Center"
+    @canonical_url = help_center_article_url(@article)
+
     render inertia: "HelpCenter/Articles/Show",
            props: {
-             article: article_props(@article),
+             article: article_props(@article, include_content: true),
              related_categories: @article.category.categories_for_same_audience.map { |c| category_props(c) },
              recaptcha_site_key: user_signed_in? ? nil : GlobalConfig.get("RECAPTCHA_LOGIN_SITE_KEY")
            }
-
-    @title = "#{@article.title} - Gumroad Help Center"
-    @canonical_url = help_center_article_url(@article)
   end
 
   private
@@ -42,22 +42,5 @@ class HelpCenter::ArticlesController < HelpCenter::BaseController
       redirect_to LEGACY_ARTICLE_REDIRECTS[params[:slug]], status: :moved_permanently
     end
 
-    def category_props(category)
-      {
-        title: category.title,
-        slug: category.slug,
-        url: help_center_category_path(category),
-        audience: category.audience,
-        articles: category.articles.map { |article| article_props(article) }
-      }
-    end
 
-    def article_props(article)
-      {
-        title: article.title,
-        slug: article.slug,
-        url: help_center_article_path(article),
-        content_html: render_to_string(partial: "help_center/articles/contents/#{article.slug}", layout: false)
-      }
-    end
 end

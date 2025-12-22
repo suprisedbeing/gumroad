0c4c114338ea25831634eb3a946305226f825899
Amar <rn.drive.2024.1@gmail.com>
2025-12-23 02:11:24 +0530

    using conditioanl sharing

--- app/controllers/concerns/inertia_rendering.rb
+++ app/controllers/concerns/inertia_rendering.rb
@@ -7,7 +7,7 @@ module InertiaRendering
   included do
     inertia_share do
       RenderingExtension.custom_context(view_context).merge(
-        current_user: current_user_props(current_user, impersonated_user),
+        current_user: (current_user_props(current_user, impersonated_user) if current_user),
         authenticity_token: form_authenticity_token,
         flash: inertia_flash_props,
         title: @title
